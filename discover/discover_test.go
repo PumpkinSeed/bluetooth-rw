54692d618d8b39edc5a635697cab95e3a0c42801
Ferenc Fabian <qwer.kocka@gmail.com>
2022-08-03 15:58:03 +0100

    Add more issue

--- discover/discover_test.go
+++ discover/discover_test.go
@@ -3,16 +3,13 @@ package discover
 import (
 	"fmt"
 	"testing"
-	"unsafe"
 )
 
 func TestScanner(t *testing.T) {
-	q := WSAQUERYSET{
-		NameSpace: 16,
-	}
+
 	fmt.Println("test start")
-	q.Size = uint32(unsafe.Sizeof(q))
-	err := Scan(q)
+
+	err := Scan()
 	fmt.Println("-----------test")
 	if err != nil {
 		t.Fatal(err)
